740539289004bdaaafd14515209d8acbc519e5b7
saqura <saqura@saqura.xyz>
2016-02-05 22:13:45 +0100

    Bump version to 0.2.2

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
 
 setup(
     name='xmppwb',
-    version='0.2.1',
+    version='0.2.2',
     description='XMPP Webhook Bridge',
     long_description=long_description,
 
